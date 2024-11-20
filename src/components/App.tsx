d6bbfc7526fa28417b36d1af0ba27aead143e90e
Ricardo Pabón <rpabon257@gmail.com>
2024-11-20 15:01:58 +0100

    chore: rename folder and paths

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { PodcastList } from '@/pages/PodcastList';
-import { PodcastEpisodeDetails } from '@/components/PodcastEpisodeDetails';
-import { PodcastEpisodeList } from '@/components/PodcastEpisodeList';
-import { PodcastLayout } from '@/components/PodcastLayout';
+import { PodcastList } from '@/routes/PodcastList';
+import { SinglePodcastOverview } from '@/routes/SinglePodcastOverview';
+import { PodcastEpisodeDetails } from '@/routes/PodcastEpisodeDetails';
+import { PodcastEpisodeList } from '@/routes/PodcastEpisodeList';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<PodcastList />} />
-        <Route path="/podcast/:podcastId" element={<PodcastLayout />}>
+        <Route path="/podcast/:podcastId" element={<SinglePodcastOverview />}>
           <Route index element={<PodcastEpisodeList />} />
           <Route
             path="episode/:episodeId"
--- src/routes/SinglePodcastOverview/index.tsx
+++ src/routes/SinglePodcastOverview/index.tsx
@@ -3,12 +3,13 @@ import { useParams, Outlet } from 'react-router-dom';
 import { PodcastDetailSidebar } from '@/components/PodcastDetailSidebar';
 import styles from './PodcastLayout.module.css';
 
-export const PodcastLayout: React.FC = () => {
+export const SinglePodcastOverview: React.FC = () => {
   const { podcastId } = useParams<{ podcastId: string }>();
 
   return (
     <div className={styles.layout}>
       <PodcastDetailSidebar podcastId={podcastId} />
+
       <div className={styles.content}>
         <Outlet />
       </div>
