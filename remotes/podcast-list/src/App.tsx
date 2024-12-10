744e1064afd27a984f482e49f13036c6cef9e373
Ricardo Pabón <rpabon257@gmail.com>
2024-12-10 11:17:39 +0100

    chore: fix podcast list app tests

--- remotes/podcast-list/src/App.tsx
+++ remotes/podcast-list/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PodcastList from './PodcastList';
+import podcastFeed from './mocks/podcast-feed.mock';
 
 const App: React.FC = () => {
   return (
     <PodcastList
-      podcastList={[]}
-      onPodcastClick={() => {
-        console.log('');
+      podcastList={podcastFeed.feed?.entry || []}
+      onPodcastClick={(podcastId: string) => {
+        console.log({ podcastId });
       }}
     />
   );
--- remotes/podcast-list/src/PodcastList/PodcastList.test.tsx
+++ remotes/podcast-list/src/PodcastList/PodcastList.test.tsx
@@ -1,94 +1,72 @@
-// import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-// import mockPodcastFeed from '@/mocks/podcast-feed.mock';
-// import { usePodcastListLogic } from '../../../../host/src/routes/PodcastList/usePodcastListLogic';
-// import PodcastList from '.';
+import { render, screen, fireEvent } from '@testing-library/react';
+import mockPodcastFeed from '@/mocks/podcast-feed.mock';
+import PodcastList from '.';
 
-// jest.disableAutomock();
-// jest.mock('./usePodcastListLogic');
+jest.disableAutomock();
 
-// jest.mock('@/hooks/usePodcastDetails', () => ({
-//   usePodcastDetails: () => ({ fetchPodcastDetails: jest.fn() }),
-// }));
+describe('PodcastList', () => {
+  const podcastList = mockPodcastFeed.feed?.entry || [];
+  const mockOnPodcastClick = jest.fn();
 
-// jest.mock('@/components/PodcastFilter/usePodcastFilterLogic', () => ({
-//   usePodcastFilterLogic: () => ({
-//     filterText: '',
-//     handleFilterChange: jest.fn(),
-//   }),
-// }));
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-// const mockNavigateToPodcast = jest.fn();
-// jest.mock('@/components/PodcastCard/usePodcastCard', () => ({
-//   usePodcastCard: () => mockNavigateToPodcast,
-// }));
+  it('should render the correct number of podcast cards based on filtered podcasts', () => {
+    const mockFilteredPodcasts = podcastList.slice(0, 2);
 
-// describe('PodcastList', () => {
-//   beforeEach(() => {
-//     jest.clearAllMocks();
-//   });
+    render(
+      <PodcastList
+        podcastList={mockFilteredPodcasts}
+        onPodcastClick={mockOnPodcastClick}
+      />,
+    );
 
-//   it('should render the correct number of podcast cards based on filtered podcasts', () => {
-//     // Get the first two podcasts from the mock feed as the filtered podcasts
-//     const mockFilteredPodcasts = mockPodcastFeed.feed?.entry.slice(0, 2);
-//     (usePodcastListLogic as jest.Mock).mockReturnValue({
-//       podcastFeed: mockPodcastFeed,
-//       filteredPodcasts: mockFilteredPodcasts,
-//       onFilterChange: jest.fn(),
-//     });
+    const podcastCards = screen.getAllByTestId('podcast-card');
+    expect(podcastCards).toHaveLength(2);
 
-//     render(<PodcastList />);
+    const filteredPodcastCount = screen.getByTestId('podcast-filter__count');
+    expect(filteredPodcastCount).toHaveTextContent('2');
+  });
 
-//     const podcastCards = screen.getAllByTestId('podcast-card');
-//     expect(podcastCards).toHaveLength(2);
+  it('should update the filter count when the filtered podcasts change', () => {
+    const { rerender } = render(
+      <PodcastList
+        podcastList={podcastList.slice(0, 2)}
+        onPodcastClick={mockOnPodcastClick}
+      />,
+    );
 
-//     const filteredPodcastCount = screen.getByTestId('podcast-filter__count');
-//     expect(filteredPodcastCount).toHaveTextContent('2');
-//   });
+    let filteredPodcastCount = screen.getByTestId('podcast-filter__count');
+    expect(filteredPodcastCount).toHaveTextContent('2');
 
-//   it('should update the filter count when the filtered podcasts change', async () => {
-//     const mockFilteredPodcasts = mockPodcastFeed.feed?.entry.slice(0, 2);
-//     (usePodcastListLogic as jest.Mock).mockReturnValue({
-//       podcastFeed: mockPodcastFeed,
-//       filteredPodcasts: mockFilteredPodcasts,
-//       onFilterChange: jest.fn(),
-//     });
+    rerender(
+      <PodcastList
+        podcastList={podcastList.slice(0, 3)}
+        onPodcastClick={mockOnPodcastClick}
+      />,
+    );
 
-//     const { rerender } = render(<PodcastList />);
+    filteredPodcastCount = screen.getByTestId('podcast-filter__count');
+    expect(filteredPodcastCount).toHaveTextContent('3');
+  });
 
-//     let filteredPodcastCount = screen.getByTestId('podcast-filter__count');
-//     expect(filteredPodcastCount).toHaveTextContent('2');
+  it('should navigate to the corresponding podcast episode on podcast card click', () => {
+    const mockFilteredPodcasts = podcastList.slice(0, 2);
 
-//     const updatedMockFilteredPodcasts = mockPodcastFeed.feed?.entry.slice(0, 3);
-//     (usePodcastListLogic as jest.Mock).mockReturnValue({
-//       podcastFeed: mockPodcastFeed,
-//       filteredPodcasts: updatedMockFilteredPodcasts,
-//       onFilterChange: jest.fn(),
-//     });
+    render(
+      <PodcastList
+        podcastList={mockFilteredPodcasts}
+        onPodcastClick={mockOnPodcastClick}
+      />,
+    );
 
-//     rerender(<PodcastList />);
+    // Find the first podcast card by its title
+    const firstPodcastTitle = mockFilteredPodcasts[0]['im:name'].label;
+    const firstPodcastCard = screen.getByText(firstPodcastTitle);
+    fireEvent.click(firstPodcastCard);
 
-//     await waitFor(() => {
-//       const filteredPodcastCount = screen.getByTestId('podcast-filter__count');
-//       expect(filteredPodcastCount).toHaveTextContent('3');
-//     });
-//   });
-
-//   it('should navigate to the corresponding podcast episode on podcast card click', () => {
-//     const mockFilteredPodcasts = mockPodcastFeed.feed?.entry.slice(0, 2) || [];
-//     (usePodcastListLogic as jest.Mock).mockReturnValue({
-//       podcastFeed: { feed: { entry: mockFilteredPodcasts } },
-//       filteredPodcasts: mockFilteredPodcasts,
-//       onFilterChange: jest.fn(),
-//     });
-
-//     render(<PodcastList />);
-
-//     // Find the first podcast card by its title
-//     const firstPodcastTitle = mockFilteredPodcasts[0]['im:name'].label;
-//     const firstPodcastCard = screen.getByText(firstPodcastTitle);
-//     fireEvent.click(firstPodcastCard);
-
-//     const expectedPodcastId = mockFilteredPodcasts[0].id.attributes['im:id'];
-//     expect(mockNavigateToPodcast).toHaveBeenCalledWith(expectedPodcastId);
-//   });
-// });
+    const expectedPodcastId = mockFilteredPodcasts[0].id.attributes['im:id'];
+    expect(mockOnPodcastClick).toHaveBeenCalledWith(expectedPodcastId);
+  });
+});
