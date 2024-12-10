556af1411becc9f31f21a313b8f26ec5006d6d67
Ricardo Pabón <rpabon257@gmail.com>
2024-12-10 11:33:42 +0100

    chore: fix podcast episode list app tests

--- remotes/podcast-episode-list/src/App.tsx
+++ remotes/podcast-episode-list/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { PodcastEpisode } from 'podcast-types';
 import PodcastEpisodeList from './PodcastEpisodeList';
+import podcastEpisodesResults from './mocks/podcast-episodes.mock.json';
 
 const App: React.FC = () => {
+  const episodes = podcastEpisodesResults.results as PodcastEpisode[];
+
   return (
     <PodcastEpisodeList
-      episodes={[]}
-      navigateToEpisode={()=> null}
+      episodes={episodes}
+      navigateToEpisode={(episodeId: number) => {
+        console.log({ episodeId });
+      }}
     />
   );
 };
--- remotes/podcast-episode-list/src/PodcastEpisodeList/PodcastEpisodeList.test.tsx
+++ remotes/podcast-episode-list/src/PodcastEpisodeList/PodcastEpisodeList.test.tsx
@@ -1,44 +1,41 @@
-// import { render, screen, fireEvent } from '@testing-library/react';
-// import podcastDetailsMock from '@/mocks/podcast-details.mock';
-// import { usePodcastEpisodeList } from './usePodcastEpisodeList';
-// import { usePodcastEpisode } from './usePodcastEpisode';
-// import { PodcastEpisodeList } from '.';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { PodcastEpisode } from 'podcast-types';
+import podcastEpisodesResults from '../mocks/podcast-episodes.mock.json';
+import PodcastEpisodeList from '.';
 
-// jest.mock('./usePodcastEpisodeList');
-// jest.mock('./usePodcastEpisode');
+describe('PodcastEpisodeList', () => {
+  const episodes = podcastEpisodesResults.results as PodcastEpisode[];
+  const resultCount = podcastEpisodesResults.resultCount as number;
+  const mockNavigateToEpisode = jest.fn();
 
-// describe('PodcastEpisodeList', () => {
-//   it('should display the correct number of episodes in the header', () => {
-//     (usePodcastEpisodeList as jest.Mock).mockReturnValue({
-//       podcastId: '934552872',
-//       episodes: podcastDetailsMock.results,
-//     });
-//     (usePodcastEpisode as jest.Mock).mockReturnValue(jest.fn());
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-//     render(<PodcastEpisodeList />);
+  it('should display the correct number of episodes in the header', () => {
+    render(
+      <PodcastEpisodeList
+        episodes={episodes}
+        navigateToEpisode={mockNavigateToEpisode}
+      />,
+    );
 
-//     const episodeHeader = screen.getByText(/Episodes:/);
-//     expect(episodeHeader).toHaveTextContent(
-//       `Episodes: ${podcastDetailsMock.resultCount}`,
-//     );
-//   });
+    const episodeHeader = screen.getByText(/Episodes:/);
+    expect(episodeHeader).toHaveTextContent(`Episodes: ${resultCount}`);
+  });
 
-//   it('should display episode titles as clickable links', () => {
-//     const episodes = podcastDetailsMock.results;
-//     (usePodcastEpisodeList as jest.Mock).mockReturnValue({
-//       podcastId: 'mockPodcastId',
-//       episodes,
-//     });
+  it('should display episode titles as clickable links', () => {
+    render(
+      <PodcastEpisodeList
+        episodes={episodes}
+        navigateToEpisode={mockNavigateToEpisode}
+      />,
+    );
 
-//     const mockNavigateToEpisode = jest.fn();
-//     (usePodcastEpisode as jest.Mock).mockReturnValue(mockNavigateToEpisode);
-
-//     render(<PodcastEpisodeList />);
-
-//     episodes.forEach((episode) => {
-//       const episodeLink = screen.getByText(episode.trackName);
-//       fireEvent.click(episodeLink);
-//       expect(mockNavigateToEpisode).toHaveBeenCalledWith(episode.trackId);
-//     });
-//   });
-// });
+    episodes.forEach((episode) => {
+      const episodeLink = screen.getByText(episode.trackName);
+      fireEvent.click(episodeLink);
+      expect(mockNavigateToEpisode).toHaveBeenCalledWith(episode.trackId);
+    });
+  });
+});
